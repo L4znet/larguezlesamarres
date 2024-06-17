a0335c9c95bede4be85ecf6ee57509851646a6ae
Charly Escalona <charly.escalona1@gmail.com>
2024-06-17 18:26:34 +0200

    :building_construction: - Creating some screens

---
+++ Screens/FeedScreen.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native";
+
+const FeedScreen = () => {
+return (
+        <View>
+            <Text>Feed Screen</Text>
+        </View>
+    )
+}
+
+export default FeedScreen;
