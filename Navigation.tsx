99f24fcd716370568e8b83180d67c3072999d263
Charly Escalona <charly.escalona1@gmail.com>
2024-06-18 11:02:29 +0200

    :lipstick: - Added some style for all headerbar

--- Navigation.tsx
+++ Navigation.tsx
@@ -3,6 +3,8 @@ import { createStackNavigator } from "@react-navigation/stack"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import FeedScreen from "./Screens/FeedScreen"
 import TabBar from "./Components/TabBar"
+import LoginScreen from "./Screens/LoginScreen"
+import RegisterScreen from "./Screens/RegisterScreen"
 
 const Navigation = () => {
      const Stack = createStackNavigator()
@@ -35,8 +37,21 @@ const Navigation = () => {
      }
 
      return (
-          <Stack.Navigator>
-               <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Navigator
+               screenOptions={{
+                    headerStyle: {
+                         backgroundColor: "#fd5353",
+                    },
+                    headerTintColor: "#fff",
+                    headerTitleStyle: {
+                         fontWeight: "bold",
+                    },
+                    headerBackTitle: "Retour",
+               }}
+          >
+               <Stack.Screen name="Home" component={Home} options={{ headerShown: false, headerTitle: "Feed" }} />
+               <Stack.Screen name="Login" component={LoginScreen} options={{ headerTitle: "Connexion" }} />
+               <Stack.Screen name="Register" component={RegisterScreen} options={{ headerTitle: "Inscription" }} />
           </Stack.Navigator>
      )
 }
