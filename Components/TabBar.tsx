88c10e4100527f718c0e311d8c8db32ff0e10d80
Charly Escalona <charly.escalona1@gmail.com>
2024-06-19 14:22:51 +0200

    :lipstick: - Changed the way we display icon and label for TabBarbutton

--- Components/TabBar.tsx
+++ Components/TabBar.tsx
@@ -1,5 +1,6 @@
 import { TouchableOpacity, View, StyleSheet, Text } from "react-native"
 import TabBarButton from "./TabBarButton"
+import { useEffect } from "react"
 
 type TabBarProps = {
      isAuthenticated: boolean
@@ -10,14 +11,16 @@ const TabBar = (props: TabBarProps) => {
           <View style={styles.bottomBar}>
                {props.isAuthenticated && (
                     <>
-                         <TabBarButton label={"Feed"} screen={"Home"} />
+                         <TabBarButton icon={"Feed"} label={"Feed"} screen={"Home"} />
+                         <TabBarButton icon={"Profile"} screen={"Profile"} label={"Profile"} />
+                         <TabBarButton icon={"Logout"} label={"Déconnexion"} />
                     </>
                )}
                {!props.isAuthenticated && (
                     <>
-                         <TabBarButton screen={"Login"} label={"Connexion"} />
-                         <TabBarButton screen={"Register"} label={"Inscription"} />
-                         <TabBarButton screen={"Home"} label={"Feed"} />
+                         <TabBarButton icon={"Feed"} label={"Feed"} screen={"Home"} />
+                         <TabBarButton icon={"Login"} screen={"Login"} label={"Connexion"} />
+                         <TabBarButton icon={"Register"} screen={"Register"} label={"Inscription"} />
                     </>
                )}
           </View>
