c897f7b7c4746a316aedfed584ce6e7fa7c3368c
Charly Escalona <charly.escalona1@gmail.com>
2024-06-26 11:05:49 +0200

    :lipstick: - Added some tabs for feed screen

---
+++ Screens/OwnoffersScreen.tsx
@@ -0,0 +1,38 @@
+import { Animated, SafeAreaView, StyleSheet, View } from "react-native"
+import Card from "../Components/Card"
+import ScrollView = Animated.ScrollView
+
+const OwnoffersScreen = () => {
+     return (
+          <SafeAreaView style={styles.container}>
+               <ScrollView style={styles.scrollView}>
+                    <View style={styles.feed}>
+                         <Card title={"Lorem ipsum"} description={""} image={require("../assets/images/bateau.jpg")} />
+                         <Card title={"Lorem ipsum"} description={""} image={require("../assets/images/bateau.jpg")} />
+                         <Card title={"Lorem ipsum"} description={""} image={require("../assets/images/bateau.jpg")} />
+                         <Card title={"Lorem ipsum"} description={""} image={require("../assets/images/bateau.jpg")} />
+                         <Card title={"Lorem ipsum"} description={""} image={require("../assets/images/bateau.jpg")} />
+                    </View>
+               </ScrollView>
+          </SafeAreaView>
+     )
+}
+
+const styles = StyleSheet.create({
+     container: {
+          flex: 1,
+          alignItems: "center",
+          width: "100%",
+     },
+
+     scrollView: {
+          flex: 1,
+          width: "100%",
+     },
+
+     feed: {
+          alignItems: "center",
+     },
+})
+
+export default OwnoffersScreen
