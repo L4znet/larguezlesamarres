47ac6ab8f2a41a10ff712c8d9c3b6bf60bc71911
Charly Escalona <charly.escalona1@gmail.com>
2024-07-12 07:38:42 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -15,6 +15,9 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
           persistSession: true,
           detectSessionInUrl: false,
      },
+     db: {
+          schema: "public",
+     },
 })
 
 AppState.addEventListener("change", (state) => {
